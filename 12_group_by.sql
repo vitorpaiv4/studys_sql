4f01ae6f92baef1f96e9452c2332088058c6f878
vitorpaiv4 <paivavitorr@gmail.com>
2026-01-02 21:41:44 -0400

    exercicio group by e having

---
+++ 12_group_by.sql
@@ -0,0 +1,30 @@
+--SELECT IdProduto,
+--       count(*) AS total_vendas 
+--FROM transacao_produto
+--GROUP BY IdProduto
+SELECT idCliente,
+      Sum(QtdePontos) AS TotalPontos,
+      count(IdTransacao) AS TotalTransacoes
+
+FROM transacoes
+WHERE Dtcriacao >= '2024-01-01'
+  AND DtCriacao < '2024-03-01'
+
+GROUP BY IdCliente
+ORDER BY sum(QtdePontos) DESC
+LIMIT 1;
+----------------------------------------
+SELECT idCliente,
+    sum(qtdePontos) AS TotalPontos,
+   count(DISTINCT IdCliente) AS TotalClientes
+
+ FROM clientes
+ WHERE DtCriacao >= '2024-01-01'
+   AND DtCriacao < '2024-03-01'
+   GROUP BY IdCliente
+   HAVING sum(QtdePontos) > 4000
+
+
+
+   ORDER BY sum(QtdePontos) DESC
+LIMIT 5;
